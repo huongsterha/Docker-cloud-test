fd0f5f4355f27eec95674fa46c2dca73c525f642
huongsterha <huongsterha@gmai.com>
2017-04-10 21:29:02 -0400

    More Changes

--- unh698.py
+++ unh698.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import 
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,9 +7,4 @@ def hello_world():
     return 'UNH698 Website'
 
 if __name__== '_main_':
-	port = int(ubuntu.environ.get('PORT', 5000))
-
-	if port == 5000:
-		app.debug = True
-
-	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=5000)
