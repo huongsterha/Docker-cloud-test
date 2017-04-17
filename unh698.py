9381759523fa412bf55107fd0e3f5b5ba6af64db
huongsterha <huongsterha@gmai.com>
2017-04-17 19:48:28 -0400

    Add coffee files

--- unh698.py
+++ unh698.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'UNH698 Website'
     return render_template('coffee.html')
+    return render_template('decafCoffee.html')
 
 if __name__== '__main__':
 	app.run(debug=True,host='0.0.0.0',port=8080)
