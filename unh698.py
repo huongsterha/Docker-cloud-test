c72bf8ece5c1b0f89c5505290fa91a0c9306c665
huongsterha <huongsterha@gmai.com>
2017-04-17 19:31:11 -0400

    rendered html

--- unh698.py
+++ unh698.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'UNH698 Website'
+    return render_template('coffee.html')
 
 if __name__== '__main__':
 	app.run(debug=True,host='0.0.0.0',port=8080)
