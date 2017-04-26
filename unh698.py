871217a81a7f599ff3a6462243c65119f13d8ca6
huongsterha <huongsterha@gmai.com>
2017-04-26 04:34:59 -0400

    This is for the staging

--- unh698.py
+++ unh698.py
@@ -4,3 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('main.html')
+    
+@app.route('/Coffee')
+def coffee():
+    return render_template('coffee.html')
+    
+@app.route('/coffeeInfo')
+def linktocoffee():
+    return render_template('decafCoffee.html')
+
+if __name__== '__main__':
+    app.run(debug=True,host='0.0.0.0',port=8080)
