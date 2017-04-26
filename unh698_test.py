733174c95b2ce5dae34c08e8b210686763843787
huongsterha <huongsterha@gmai.com>
2017-04-26 02:38:07 -0400

    Added a main page

--- unh698_test.py
+++ unh698_test.py
@@ -35,16 +35,16 @@ class FlaskrTestCase(unittest.TestCase):
         assert b'UNH698 Website' in rv.data
         
     def test_link_to_my_page(self):
-        rv = self.app.get('/')  
+        rv = self.app.get('/Coffee')  
         # Search the page contents for the link to your topic page 
         # Replace xxxxxxxxxxxx with text you'd expect to see on your main page that links to your subpage
         assert b'Coffee' in rv.data 
 
     def test_my_topic(self):
         # Replace '/' with the page path you want to make
-        rv = self.app.get('/Coffee')  
+        rv = self.app.get('/coffeInfo')  
         # Replace UNH698 Website with the text you expect to see on you topic page
-        assert b'Coffee' in rv.data 
+        assert b'Information on coffee' in rv.data 
 
 
 
