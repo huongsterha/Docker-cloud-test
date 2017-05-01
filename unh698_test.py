8aea70e08982f9896ba7ec15987c84c4760421cc
huongsterha <huongsterha@gmail.com>
2017-05-01 23:58:05 +0000

    Hyerlink works now

--- unh698_test.py
+++ unh698_test.py
@@ -42,7 +42,7 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_my_topic(self):
         # Replace '/' with the page path you want to make
-        rv = self.app.get('/coffeeInfo')  
+        rv = self.app.get('/Coffee/coffeeInfo')  
         # Replace UNH698 Website with the text you expect to see on you topic page
         assert b'Coffee' in rv.data 
 
