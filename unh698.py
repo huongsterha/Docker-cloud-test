596171b4dd82cf8656ec7e78a482cff4a6d4272b
huongsterha <huongsterha@gmail.com>
2017-05-01 23:26:16 +0000

    setup_metics put after app = Flask(__name__)

--- unh698.py
+++ unh698.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from prometheus_metrics import setup_metrics
-setup_metrics(app)
 
 app = Flask(__name__)
+setup_metrics(app)
 
 @app.route('/')
 def main():
